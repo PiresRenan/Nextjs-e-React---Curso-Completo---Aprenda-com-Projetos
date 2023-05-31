2a813f752c90412e8210aa503703d071c6fce57b
Renan Dev <renan.sp.121@hotmail.com>
2023-05-31 12:26:24 -0300

    [31/05/2023]

---
+++ Porta Premiada - Aplicando os Conceitos Fundamentais/portas/src/pages/index.jsx
@@ -0,0 +1,18 @@
+
+import Porta from "@/components/Porta";
+import Presente from "@/components/Presente";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Portas surpresa</title>
+      </Head>
+      <main>
+        <Porta />
+      </main>
+    </>
+  )
+}
+
